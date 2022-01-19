3e5e86282e856228080a98a098b8e635f0e61690
richasinhaa <richa.sinha0603@gmail.com>
2022-01-19 14:07:13 +0800

    Cleartrip Clone App Template

---
+++ src/components/Sidebar/SidebarData.js
@@ -0,0 +1,20 @@
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+
+export const SidebarData = [
+{
+	title: "Flights",
+	path: "/",
+	icon: <FaIcons.FaPlane />,
+},
+{
+	title: "About Us",
+	path: "/about",
+	icon: <FaIcons.FaUsers />,
+},
+{
+	title: "Contact",
+	path: "/contact",
+	icon: <FaIcons.FaPhone />,
+},
+];
